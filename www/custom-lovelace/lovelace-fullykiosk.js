adcf1107c9d4615797b02ffb82afa9c24e2d20b6
Pierre <3458055+BaQs@users.noreply.github.com>
2019-05-08 19:03:25 +0200

    updated custom lovelace cards

--- www/custom-lovelace/lovelace-fullykiosk.js
+++ www/custom-lovelace/lovelace-fullykiosk.js
@@ -1,4 +1,4 @@
-var FullyKiosk = FullyKiosk || (function() {
+var LovelaceFullyKiosk = LovelaceFullyKiosk || (function() {
 
   var _active = false
   var _screen;
@@ -10,9 +10,9 @@ var FullyKiosk = FullyKiosk || (function() {
   var _sync_timer;
 
   _register = function() {
-    fully.bind('screenOn', 'FullyKiosk.onScreenOn();');
-    fully.bind('screenOff', 'FullyKiosk.onScreenOff();');
-    fully.bind('onMotion', 'FullyKiosk.onMotion();');
+    fully.bind('screenOn', 'LovelaceFullyKiosk.onScreenOn();');
+    fully.bind('screenOff', 'LovelaceFullyKiosk.onScreenOff();');
+    fully.bind('onMotion', 'LovelaceFullyKiosk.onMotion();');
     let hass = document.querySelector('home-assistant').hass;
     hass.connection.subscribeEvents((event)=> {
       if(event.data.domain === 'light' && event.data.service_data.entity_id.toString() === _screen) {
@@ -34,14 +34,15 @@ var FullyKiosk = FullyKiosk || (function() {
 
   _sendScreenState = function() {
     let hass = document.querySelector('home-assistant').hass;
-    hass.callApi('post', "states/"+_screen, {
-      state: _screen_state?'on':'off',
-      attributes: {
+    let attr = Object.assign(hass.states[_screen].attributes, {
         brightness: fully.getScreenBrightness()*255/170,
         supported_features: 1,
         battery_level: fully.getBatteryLevel(),
         charging: fully.isPlugged(),
-      },
+      });
+    hass.callApi('post', "states/"+_screen, {
+      state: _screen_state?'on':'off',
+      attributes: attr,
     });
   }
 
@@ -49,12 +50,14 @@ var FullyKiosk = FullyKiosk || (function() {
     clearTimeout(_sync_timer);
     let timeout = _motion_state?5000:10000;
     let hass = document.querySelector('home-assistant').hass;
-    hass.callApi('post', "states/"+_motion, {
-      state: _motion_state?'on':'off',
-      attributes: {
+    let attr = Object.assign(hass.states[_motion].attributes, {
         battery_level: fully.getBatteryLevel(),
         charging: fully.isPlugged(),
-      },
+        device_class: 'motion',
+    });
+    hass.callApi('post', "states/"+_motion, {
+      state: _motion_state?'on':'off',
+      attributes: attr,
     });
     _sendScreenState();
     _sync_timer = setTimeout(() => {
---
+++ www/kiosk-config.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+  // LovelaceFullyKiosk.bind("d646dfbb-c10b45d0", "light.dashboard_screen", "binary_sensor.dashboard_motion");
+  LovelaceFullyKiosk.bind("d646dfbb-c10b45d0", "light.dashboard_screen");
+}, 200);
